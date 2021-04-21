0137ff015f2d5b0544f84066d4a035a02492a868
gakunkel <gkunkel@ucsd.edu>
2021-04-20 21:35:17 -0700

    Final Tuesday commit

--- index.js
+++ index.js
@@ -2,9 +2,10 @@
 d3.json("http://localhost:5000/api/battles").then(function(data) {
     console.log(data)
     var layout = {
-        'paper_bgcolor': 'rgba(255,255,255,0.4)',
+        'paper_bgcolor': 'rgba(255,255,255,0)',
         "title": "G.o.T. Kings: Attacks vs. Defenses",
         "barmode": "group",
+        "color": "#d3d3d3",
     };
 
     Plotly.newPlot("plot", data, layout);
@@ -14,9 +15,10 @@ d3.json("http://localhost:5000/api/battles").then(function(data) {
 d3.json("http://localhost:5000/api/directors").then(function(data) {
     console.log(data)
     var layout = {
-        'paper_bgcolor': 'rgba(255,255,255,0.4)',
+        'paper_bgcolor': 'rgba(255,255,255,0)',
         "title": "Directors vs. Ratings",
-        "barmode": "group"
+        "barmode": "group",
+        "color": "#d3d3d3"
         
     };
 
