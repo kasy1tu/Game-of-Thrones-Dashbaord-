9f11051425e0858e7946f102de72279fe012d7d6
kasy1tu <74841065+kasy1tu@users.noreply.github.com>
2021-04-22 20:19:40 -0700

    progress check

--- index.js
+++ index.js
@@ -121,6 +121,57 @@ d3.json("http://localhost:5000/api/directors").then(function(data) {
     
 });
 
+d3.csv("got_cleaned.csv", function(err, rows){
+
+  function unpack(rows, key) {
+  return rows.map(function(row) { return row[key]; });
+  }
+
+  var headerNames = d3.keys(rows[0]);
+
+  var headerValues = [];
+  var cellValues = [];
+  for (i = 0; i < headerNames.length; i++) {
+    headerValue = [headerNames[i]];
+    headerValues[i] = headerValue;
+    cellValue = unpack(rows, headerNames[i]);
+    cellValues[i] = cellValue;
+  }
+
+  // clean date
+  for (i = 0; i < cellValues[1].length; i++) {
+  var dateValue = cellValues[1][i].split(' ')[0]
+  cellValues[1][i] = dateValue
+  }
+
+
+var data = [{
+  type: 'table',
+//   columnwidth: [150,600,1000,900,600,500,1000,1000,1000],
+  columnorder: [0,1,2,3,4,5],
+  header: {
+    values: headerValues,
+    align: "center",
+    line: {width: 1, color: 'rgb(50, 50, 50)'},
+    fill: {color: ['rgb(235, 100, 230)']},
+    font: {family: "Arial", size: 8, color: "white"}
+  },
+  cells: {
+    values: cellValues,
+    align: ["center", "center"],
+    line: {color: "black", width: 1},
+    fill: {color: ['rgba(228, 222, 249, 0.65)','rgb(235, 193, 238)', 'rgba(228, 222, 249, 0.65)']},
+    font: {family: "Arial", size: 9, color: ["black"]}
+  }
+}]
+
+var layout = {
+  title: "Game of Thrones Top Rated Episodes"
+}
+
+Plotly.newPlot('myDiv', data, layout);
+});
+
 
 
 
